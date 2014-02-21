a43797510781491c290acc8b544bdb159b45baeb
Natalie Kathryn Blackburn & Anne Jhetson <student@epicodus.com>
2014-02-20 16:23:51 -0800

    creating recurring version of factorial and housecleaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,45 @@
-var factorialFinder = function(numberToFactorial) {
-  var placeholderValue = 1;
+var recurringFactorialFinder = function(numberToFactorial) {
 
   if (isNaN(numberToFactorial)){
     return "Please enter a numerical value.";
 
   } else if (numberToFactorial === 0) {
-    console.log(numberToFactorial);
     return 1;
 
   } else if (numberToFactorial < 0) {
     return "Please enter a positive intiger.";
 
-  } else {
-      for (numberToFactorial; numberToFactorial > 1; numberToFactorial -= 2) {
-        placeholderValue = placeholderValue * (numberToFactorial * (numberToFactorial - 1));
-      }
-    return placeholderValue;  
-  }
+  } else if (numberToFactorial >= 0) {
+    return  numberToFactorial * recurringFactorialFinder(numberToFactorial - 1);
+    }
 };
 
+var loopFactorialFinder = function (numberToFactorial) {
+var placeholderValue = 1;
+
+  if (isNaN(numberToFactorial)){
+    return "Please enter a numerical value.";
+
+  } else if (numberToFactorial === 0) {
+    return 1;
+
+  } else if (numberToFactorial < 0) {
+    return "Please enter a positive intiger.";
+
+  } for (numberToFactorial; numberToFactorial > 1; numberToFactorial -= 2) {
+      placeholderValue = placeholderValue * (numberToFactorial * (numberToFactorial - 1));
+    }
+    return placeholderValue;
+};
 
 $(document).ready(function() {
   $("form#value-input").submit(function(event) {
 
     var numberToFactorial = parseInt($("input#factorial-number").val());
-    var factorialValue = factorialFinder(numberToFactorial);
+    var factorialValue = recurringFactorialFinder(numberToFactorial);
 
     $("#final-value").text(factorialValue);
     $("#result").show();
-
-
 
     event.preventDefault();
 
